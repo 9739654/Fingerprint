ab7ba8660058cea5dca50a7bb8975808eb4f2dba
Mateusz Paluch <9739654@gmail.com>
2015-03-05 10:41:40 +0100

    Initial commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,31 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+
 public class Controller {
+	@FXML
+	MenuItem mniFileOpen, mniFileExit;
+	@FXML
+	Button btnFilter1, btnFilter2;
+	@FXML
+	ImageView imgLeft, imgRight;
+
+	void handleFilter1(ActionEvent event) {
+
+	}
+
+	void handleFilter2(ActionEvent event) {
+
+	}
+
+	void handleFileOpen(ActionEvent event) {
+
+	}
+
+	void handleFileExit(ActionEvent event) {
+
+	}
 }
